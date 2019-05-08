73fd3e90aadfc3d35ef829da9db5fd7ad0989941
mikey <manicdesigner@gmail.com>
2019-05-07 21:22:57 -0400

    feat(Article.js) says Close or Expand Expand or Close depending on article-open

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
@@ -15,6 +15,11 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    if (this.domElement.classList.contains('article-open')) {
+      this.expandButton.textContent = 'Close';
+    } else {
+      this.expandButton.textContent = 'Expand';
+    }
   }
 }
 
