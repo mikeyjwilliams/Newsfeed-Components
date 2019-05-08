ab8947d90462d4b6cfbd1b48fff2545217b49e60
mikey <manicdesigner@gmail.com>
2019-05-07 23:08:43 -0400

    feat(slide animate article) article animate slide up

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'Expand';
+    this.expandButton.innerText = 'Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
@@ -16,10 +16,16 @@ class Article {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
     if (this.domElement.classList.contains('article-open')) {
-      this.expandButton.textContent = 'Close';
-    } else {
-      this.expandButton.textContent = 'Expand';
+      $('.article-open').animate({
+        height: "400px"
+      });
     }
+    if (!this.domElement.classList.contains('article-open')) {
+      $('.article').animate({
+        height: "50px"
+      });
+    }
+
   }
 }
 
